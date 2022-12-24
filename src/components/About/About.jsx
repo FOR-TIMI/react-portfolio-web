247be910af9efcab4d5cced5eaecb414f887a25d
FOR-TIMI <timiplayskeys@gmail.com>
2022-12-24 10:15:42 -0500

    added about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './about.css'
+import { FaAward, FaTelegramPlane } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { BsClipboardCheck } from 'react-icons/bs';
+import Skill from './Skill';
 
 const About = () => {
   return (
@@ -9,10 +13,41 @@ const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-
+              <Skill/>
         </div>
         <div className="about__content">
-          
+          <div className="about__cards">
+            
+            {/* Experience  */}
+            <article className="about__card">
+                <FaAward className='about__icon'/>
+                <h5>Experience</h5>
+                <small>4+ years Experience</small>
+            </article>
+
+            {/* Collaborators  */}
+            <article className="about__card">
+                <FiUsers className='about__icon'/>
+                <h5>Collaborators</h5>
+                <small>30+ Worldwide</small>
+            </article>
+
+            {/* Projects  */}
+            <article className="about__card">
+                <BsClipboardCheck className='about__icon'/>
+                <h5>Projects</h5>
+                <small>50+ Completed projects</small>
+            </article>
+          </div>
+
+           <p>
+              Hello, There. I'm Femi-Ladiran Erifeoluwa but everyone calls me Timi. <br />
+              I am a Web Developer that specializes in building creative and intuitive full stack web applications.
+              <br />
+            A soccer, piano, and outdoor activity enthusiast. interested in building full stack applications and working on ambitious projects and positive people. Let's build something useful together
+           </p>
+
+           <a href='#contact' className='btn btn-primary'>Let's chat <FaTelegramPlane/></a>
         </div>
       </div>
     </section>
---
+++ src/components/About/Skill.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { TagSphere } from "react-tag-sphere";
+
+const Skill = () => {
+    const tags = [
+        'JavaScript', 'CSS', 'HTML',
+        'C++', 'React',
+       'Python', 'Java', 'git',
+       'Node.js', 'MongoDB',
+       'Express', 'MySQL', 'jQuery',
+       'jwt', 'GraphQl', 'REST',
+    ]
+
+  return (
+    <TagSphere
+    style={{
+        overflow: "hidden",
+        marginBottom: "2.5rem"
+    }}
+    tags={tags}
+    radius={230}
+  />
+   )
+}
+
+export default Skill
