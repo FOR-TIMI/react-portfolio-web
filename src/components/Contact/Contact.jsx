8b430f621c5e150c9d7f87f339c8731110b056ee
FOR-TIMI <timiplayskeys@gmail.com>
2022-12-25 13:56:11 -0500

    added contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsInstagram, BsLinkedin} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section >
+    <section id='contact'>
+      <h5>Reach out to me</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          {/* Email  */}
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>femiladiranerife24@gmail.com</h5>
+            <a href="mailto:femiladiranerife24@gmail.com" target='_blank' re="norefferer">Email me</a>
+          </article>
+
+          {/* Linkedin  */}
+          <article className="contact__option">
+            <BsLinkedin className='contact__option-icon'/>
+            <h4>Linkedin</h4>
+            <h5>Erifeoluwa Femi-Ladiran</h5>
+            <a href="https://www.linkedin.com/in/erifeoluwa-femi-ladiran/" target='_blank' rel="norefferer">Let's connect</a>
+          </article>
+
+          {/* Instagram  */}
+          <article className="contact__option">
+            <BsInstagram className='contact__option-icon'/>
+            <h4>Instagram</h4>
+            <h5>for_timi</h5>
+            <a href="https://ig.me/m/for_timi" target='_blank' rel="norefferer">Send a DM</a>
+          </article>
+
+          
+        </div>
+        
+        {/* END OF CONTACT OPTIONS  */}
+        <form action="" >
+           <input type="text" name='name' placeholder='Full Name' required/>
+           <input type="email" name='email' placeholder='Email' required/>
+           <textarea name="message" placeholder='Message' rows="7" required></textarea>
+           <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section >
   )
 }
 
