d62394d9a60b2b0bc6fb007ace54138bac2086ee
FOR-TIMI <timiplayskeys@gmail.com>
2022-12-25 17:03:12 -0500

    added favicon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
        <article className="container header__container">
          <h5>Hello I'm</h5>
-         <h1>Femi-Ladiran Erifeoluwa</h1>
+         <h1>Erifeoluwa Femi-Ladiran</h1>
          <h5 className="text-light">Full-stack Software Developer</h5>
          <CTA/>
 
