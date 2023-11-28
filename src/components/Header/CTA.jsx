9a87a8efacc6ab8436d6749ffc65199666748599
FOR-TIMI <timiplayskeys@gmail.com>
2023-11-28 01:12:17 -0500

    updated build and resume url

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import CV from '../../assets/CV.pdf'
-
+import React from "react";
 
 const CTA = () => {
   return (
-        <div className="cta">
-            <a data-aos="fade-right" href={CV} download className='btn'>Resume</a>
-            <a data-aos="fade-left" href="#contact" className='btn btn-primary'>Let's Connect</a>
-        </div>
-    )
-}
+    <div className="cta">
+      <a
+        data-aos="fade-right"
+        target="_blank"
+        href="https://docs.google.com/gview?url=https://obsidi-assets-prd.s3.amazonaws.com/uploads/eb21b7a9-133d-4ce9-85f7-d3c019b8f305/docs/resume-1701151587082.pdf"
+        className="btn"
+        rel="noreferrer"
+      >
+        Resume
+      </a>
+      <a data-aos="fade-left" href="#contact" className="btn btn-primary">
+        Let's Connect
+      </a>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
